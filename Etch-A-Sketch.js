58c0ba850f8ef2f0378d8fc8362818e8ff382d3b
christot20 <thomaschristo1234@yahoo.com>
2021-10-25 22:39:58 -0400

    Added dynamic grid

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -1,8 +1,15 @@
-gridNum = parseInt(prompt("How many grids do you want?"));
-container = document.querySelector("#gridContainer");
-for(let i = 0; i < gridNum; i++){
+let gridNum = parseInt(prompt("How many grids do you want?"));
+let container = document.querySelector("#gridContainer");
+document.documentElement.style
+        .setProperty("--columnNum", gridNum);
+document.documentElement.style
+        .setProperty("--rowNum", gridNum);
+//maybe set the column num and grid num
+// equal to the gridnum and have the amount of grids equal
+//grid num x gridnum
+for(let i = 0; i < gridNum*gridNum; i++){
     console.log(gridNum, i)
     gridDiv = document.createElement('div');
     gridDiv.textContent = "Hi";
-    container.appendChild(gridDiv);
+    container.appendChild(gridDiv);    
 }
