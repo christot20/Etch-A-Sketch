2c15405e6bf92d525f4e33360c208794b8b3b091
christot20 <thomaschristo1234@yahoo.com>
2021-10-29 15:10:32 -0400

    added rainbow feature

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -1,20 +1,71 @@
+// let container = document.querySelector('#gridContainer');
+// function gridCreate(num) {
+//   let gridNum = num;
+//   document.documentElement.style.setProperty('--columnNum', gridNum);
+//   document.documentElement.style.setProperty('--rowNum', gridNum);
+
+//   for (let i = 0; i < gridNum * gridNum; i++) {
+//     let gridDiv = document.createElement('div');
+//     gridDiv.classList.add('cell');
+//     container.appendChild(gridDiv);
+//   }
+// }
+
+// let cells = document.querySelectorAll('.cell');
+// const gridSize = document.getElementById('gridSize');
+
+// gridCreate(16);
+
+// //make rainbow
+// function rainbow(){
+//     let letters = "0123456789ABCDEF";
+//     let color = "#";
+//     for (let i = 0; i < 6; i++) {
+//         color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color
+// }
+// //add mouseover event that makes use of rainbow
+// container.addEventListener("mouseover", function(e){
+//     console.log(e);
+//     console.log(e.target.className)
+//     if(e.target.className === 'cell'){
+//         e.target.style.background = rainbow();
+//     }
+// });
+//your way
+
+// container.addEventListener('mouseover', (e) => {
+//     if (e.target.classList.contains('cell')) {
+//       e.target.style.background = rainbow();
+//     }
+//   });
+// this was his way
+
+// implement this into your code and have it work when pressing thr RGB button
+
+//look at phone to see changes u need to make (in safari)
+//fix bug where u can change color after rgb
+//maybe change hover function/how grid is made and hover is done similar to how the guy helped u (code is above)
+//see if u can make code more efficient (kind of done already)
+//might need to make abutton/state for color mode (see michaelalosmans project in the project page in odin)
+
+
+//old code
+let container = document.querySelector("#gridContainer");
 function gridCreate(){
     let gridNum = parseInt(prompt("How many grids do you want?"));
-    let container = document.querySelector("#gridContainer");
     document.documentElement.style
             .setProperty("--columnNum", gridNum);
     document.documentElement.style
             .setProperty("--rowNum", gridNum);
  
     for(let i = 0; i < gridNum*gridNum; i++){
-        console.log(gridNum, i)
         let gridDiv = document.createElement('div');
         gridDiv.classList.add("cell");
         container.appendChild(gridDiv);  
-        gridDiv.addEventListener("mouseover", hover); 
     }
     function gridChanger(e){
-        console.log(e);
         while (container.firstChild) {
             container.removeChild(container.firstChild);
         }
@@ -27,62 +78,69 @@ function gridCreate(){
 
 let cells = document.querySelectorAll(".cell"); 
 const gridSize = document.getElementById("gridSize");
-
-function hover(e){
-    if (e.target = "div.cell"){ //color chooser
-        /* if (something about eraser button being pushed){
-        e.target.style.background = "#fff"; 
+function colorMode(e){
+    // if(e.returnValue === true){
+    //     e.target.style.background = "red"; 
+    // }
+    //console.log(e);
+    container.addEventListener("mouseover", hover);
+    function hover(e){
+        if (e.target = "div.cell"){ //color chooser
+            // console.log(e.target.outerHTML);
+            // console.log(document.getElementById("colorPicker").value);
+            e.target.style.background = document.getElementById("colorPicker").value;   
+        }
+        function clear(){ //clears the grid
+            e.target.style.background = "white";
+        }
+        document.getElementById("clear").addEventListener("click",clear);
     }
-    */ //make and else for the thing under me and try it
-        // console.log(e.target.outerHTML);
-        // console.log(document.getElementById("colorPicker").value);
-        e.target.style.background = document.getElementById("colorPicker").value; 
-    }
-    function clear(){ //clears the grid
-        e.target.style.background = "white";
-    }
-    function eraser(e){ //toggles eraser
-        console.log(e);
-        document.getElementById("colorPicker").value = "#FFFFFF";
-        console.log(document.getElementById("colorPicker").value);
-    }
-    document.getElementById("clear").addEventListener("click",clear);
-    document.getElementById("erase").addEventListener("click", eraser);
 }
 
-// const erase = document.getElementById("erase");
-// erase.addEventListener("click", eraser);
+document.getElementById("colorMode").addEventListener("click", colorMode);
+document.getElementById("RGB").addEventListener("click", mainrainbow);
+document.getElementById("erase").addEventListener("click", eraserMode);
 
+function mainrainbow(){ //random color on hover
+    function rainbow(){
+        let letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color
+    }
+        //add mouseover event that makes use of rainbow
+        container.addEventListener("mouseover", function(e){
+            //console.log(e);
+            //console.log(e.target.className)
+            if(e.target.className === 'cell'){
+                e.target.style.background = rainbow();
+            }
+            // else if(e.target.className === 'cell' && eraser === "[button#erase, div#sideButtons, body, html, document, Window]"){
+            //     e.target.style.background = "#FFFFFF";
+            // }
+        });
+}
 
-//erase.addEventListener("click", hover);
-// function eraseGrid(e){
-//     function hoverwhite(){
-
-//     }
-//     console.log(e);
-//     // if (e.target = "div.cell"){
-//     //     e.target.style.background = "white";
-//     // }
-// }
-
-
-
-
-// const changeColor = document.getElementById("changeColor");
-
- 
-
-// const colorPicker = document.getElementById("colorPicker");
-// colorPicker.addEventListener("input", updateFirst, false);
-// colorPicker.addEventListener("change", watchColorPicker, false);
-
-// function watchColorPicker(event) {
-//   document.querySelectorAll("p").forEach(function(p) {
-//     p.style.color = event.target.value;
-//   });
-// }
-
+function eraserMode(){
+    container.addEventListener("mouseover", eraser);
+    function eraser(e){ //toggles eraser
+        if (e.target = "div.cell"){ //color chooser
+            // console.log(e.target.outerHTML);
+            // console.log(document.getElementById("colorPicker").value);
+            e.target.style.background = "#FFFFFF";   
+        }
+        // document.getElementById("colorPicker").value = "#FFFFFF";
+        // return e.path //have return value be if button was clicked or not
+        //remember that = is for assigning a variable to something and === is 
+        //to make sure something IS equal to something (returns true or false)
+    }
+}
 gridCreate();
+colorMode(); //make it so when a function is running the corresponding button lights up
+//also add slider for grid size
+
 //be sure to add comments when ur done with this to ALL pages
 
 //make an eraser work with someway of having the button click mean the 
@@ -92,6 +150,9 @@ gridCreate();
 
 //maybe use modal instead of prompt and possibly have the 
 //dimensions preset when loaded so it shows automatically instead of popping up
+//WORK ON RGB SETTING AND GETTING THE GRID TO LOAD 16X16 AUTOMATICALLY
+//HAVE THE GRID RESET PROMPT THE USER OR MAKE A SLIDER
+
 
 //have it show how big grid is using spans either when chooing in the modal 
 //or on the webpage or both
